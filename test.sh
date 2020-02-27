2b6d10bf61e016f05e3d6795967cdfd1c927bb2e
sariscas <saraferreira.p3@gmail.com>
2020-02-27 17:23:16 +0000

    changed to python3

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 rand_port() {
     openssl rand 2 \
-    | python -c "import sys; print(int.from_bytes(sys.stdin.buffer.read(), 'big'))"
+    | python3 -c "import sys; print(int.from_bytes(sys.stdin.buffer.read(), 'big'))"
 }
 
 port=`rand_port`
-python -m cobre.tests.basic 127.0.0.1:$port
+python3 -m cobre.tests.basic 127.0.0.1:$port
