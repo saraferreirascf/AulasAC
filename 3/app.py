90111e6a83e8165188f79f55ae4da050b83625ae
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-03-17 19:16:43 +0000

    tidy up main

--- 3/app.py
+++ 3/app.py
@@ -3,11 +3,19 @@ import sys
 import client
 import server
 
-if sys.argv[1]=="Client_RC4":
-    client.RC4().run()
-elif sys.argv[1]=="Server_RC4":
-    server.RC4().run()
-elif sys.argv[1]=="Client_AES_CBC_NoPadding":
-    client.RC4().run()
-elif sys.argv[1]=="Server_AES_CBC_NoPadding":
-    server.RC4().run()
+if __name__ == '__main__':
+    backends = {
+        # RC4 encryption
+        'Client_RC4': client.RC4,
+        'Server_RC4': server.RC4,
+        # AES in CBC mode with no particular padding scheme
+        'Client_AES_CBC_NoPadding': client.AES_CBC_NoPadding,
+        'Server_AES_CBC_NoPadding': server.AES_CBC_NoPadding,
+    }
+    backend = backends.get(sys.argv[1]) if len(sys.argv) == 2 else None
+    if not backend:
+        backends = '|'.join(backends.keys())
+        print(f'Usage: python {sys.argv[0]} <{backends}>')
+        sys.exit(1)
+    instance = backend()
+    instance.run()
