e4a562b9dde98f549552b78426c4fd3b535183ec
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-03-18 15:29:28 +0000

    small fix

--- 3/app.py
+++ 3/app.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     }
     backend = backends.get(sys.argv[1]) if len(sys.argv) == 2 else None
     if not backend:
-        print('Usage: python {sys.argv[0]} <backend>')
+        print(f'Usage: python {sys.argv[0]} <backend>')
         print('Backends:')
         for b in backends.keys():
             print(f'\t{b}')
