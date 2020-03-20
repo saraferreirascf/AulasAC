5136a0c6c4d96f86680d72f0f643186a6e27a145
sariscas <saraferreira.p3@gmail.com>
2020-03-20 23:47:29 +0000

    clean it up

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rand_port() {
-    openssl rand 2 \
-    | python3 -c "import sys; print(int.from_bytes(sys.stdin.buffer.read(), 'big'))"
-}
-
-port=`rand_port`
-python3 -m cobre.tests.basic 127.0.0.1:$port
