44d9d504658d430f033d7c3cd023da552a6302e2
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-03-27 18:04:02 +0000

    DH key exchange complete

--- 5/server.py
+++ 5/server.py
@@ -4,16 +4,15 @@ import sys
 import hashlib
 import hmac
 import json
+import base64
 
+from keyexchange import DiffieHellman
 from contextlib import closing
 from Crypto.Util import Counter
 from Crypto.Cipher import AES
 
 HOST = '127.0.0.1'  #localhost
 PORT = 45676 #non-privileged ports are > 1023
-KEY = b"very secret key"
-K1 = hashlib.sha256(KEY+b'1').digest()[:16]
-K2 = hashlib.sha256(KEY+b'2').digest()[:16]
 
 class ServerThread(threading.Thread):
     def __init__(self,crypto,c,id):
@@ -25,11 +24,14 @@ class ServerThread(threading.Thread):
 
     def run(self):
         with closing(self.c) as c:
+            print(f'[ {self.id} ]i begun key exchange')
+            self.crypto.key_exchange(self.id, c)
+            print(f'[ {self.id} ]i key exchange successful')
             while True:
                 msg = c.recv(1024)
                 if msg:
                     try:
-                        plaintext = self.crypto.dec(msg, self.seq)
+                        plaintext = self.crypto.dec(self.id, msg, self.seq)
                         self.seq = (self.seq + 1) & 0xffffffffffffffff
                         sys.stdout.buffer.write(b'[ ')
                         sys.stdout.buffer.write(bytes(str(self.id), encoding='utf8'))
@@ -38,7 +40,7 @@ class ServerThread(threading.Thread):
                     except ValueError:
                         sys.stdout.buffer.write(b'[ ')
                         sys.stdout.buffer.write(bytes(str(self.id), encoding='utf8'))
-                        sys.stdout.buffer.write(b' (error) ]: invalid mac')
+                        sys.stdout.buffer.write(b' ]!  invalid mac')
                     finally:
                         sys.stdout.buffer.write(b'\n')
                         sys.stdout.buffer.flush()
@@ -47,8 +49,20 @@ class ServerThread(threading.Thread):
                     break
 
 class Server(object):
+    def serialize(self, o):
+        return json.dumps(o).encode()
+
     def deserialize(self, x):
         return json.loads(x.decode('utf8'))
+
+    def key_exchange(self, id, c):
+        ...
+
+    def dec(self, id, p, seq):
+        return self.deserialize(p)
+
+    def unpad(self, p):
+        return p
 
     def run(self):
         order_number=0
@@ -70,15 +84,28 @@ class Server(object):
                     break
 
 class SafeServer(Server):
+    def __init__(self):
+        self.keys = {}
+
+    def key_exchange(self, id, c):
+        msg = c.recv(4096)
+        dh = DiffieHellman()
+        c.sendall(self.serialize(dh.get_public()))
+        key = dh.compute_shared_secret(self.deserialize(msg))
+        k1 = hashlib.sha256(key+b'1').digest()[:16]
+        k2 = hashlib.sha256(key+b'2').digest()[:16]
+        self.keys[id] = (k1, k2) # TODO: synchronize this with a mutex
+
     def unpad(self, p):
         return p[:-p[-1]]
 
-    def dec(self, p, seq):
+    def dec(self, id, p, seq):
+        k1, k2 = self.keys[id]
         p = self.deserialize(p)
         seq = seq.to_bytes(8, 'big')
-        mac = hmac.new(K2, p['cryptogram'].encode()+seq, hashlib.sha256).hexdigest()
+        mac = hmac.new(k2, p['cryptogram'].encode()+seq, hashlib.sha256).hexdigest()
         if not hmac.compare_digest(p['mac'], mac):
             raise ValueError('invalid mac')
-        ctr_d = Counter.new(64, prefix=bytes.fromhex(p['iv']))
-        ciph = AES.new(K1, AES.MODE_CTR, counter=ctr_d)
-        return self.unpad(ciph.decrypt(bytes.fromhex(p['cryptogram'])))
+        ctr_d = Counter.new(64, prefix=base64.decodebytes(p['iv'].encode()))
+        ciph = AES.new(k1, AES.MODE_CTR, counter=ctr_d)
+        return self.unpad(ciph.decrypt(base64.decodebytes(p['cryptogram'].encode())))
