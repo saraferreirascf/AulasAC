c9e3822092da71063f3a32e502573834c5e1f42a
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-06-22 22:41:38 +0100

    aaaaaaa

--- Project/server.py
+++ Project/server.py
@@ -124,7 +124,7 @@ def main_new_user():
     print('Enter the pin:')
     pin = sys.stdin.readline()[:-1].encode()
     userid = db.new(pin)
-    print(f'Generated user id: {str(userid, "utf8")}')
+    print(f'Generated user id: {userid}')
     db.save()
 
 def main():
